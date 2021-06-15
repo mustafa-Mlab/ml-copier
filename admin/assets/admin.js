daba4bec2082a047706f806493b2a1e1bcda59eb
mustafa-Mlab <mustofa.kamal@sekai-lab-bd.com>
2021-06-15 18:28:29 +0600

    fix: resolved timeout issue using ajax

--- admin/assets/admin.js
+++ admin/assets/admin.js
@@ -12,7 +12,7 @@
         var thisPostTypeContainer = $(this).closest('.postTyperow').find('.id-specific-section .id-specific-posts');
         $.get(url, function(data, status){
           $(data).each(function(index, value){
-            $(thisPostTypeContainer).append('<li><label><input type="checkbox" name="posts[]" value="' + value.ID + '" checked >' + value.post_title + '</label></li> ');
+            $(thisPostTypeContainer).append('<li><label><input type="checkbox" name="posts[]" class="posts" value="' + value.ID + '" checked >' + value.post_title + '</label></li> ');
           });
         });
     }else{
@@ -21,7 +21,6 @@
     });
 
     $('.get-all').change( function(e){
-      console.log(this);
       if(this.checked) {
         $(this).closest('.fetch-data').find('.id-specific-section').hide();
         $(this).closest('.fetch-data').find('.id-specific-section ul input:checkbox').each(function (index, value){
@@ -35,5 +34,25 @@
       }
     });
 
+    // Make ajax requests
+
+    $('#wp_copier_form').submit(function (e){
+      e.preventDefault();
+      $('.report').append("<h4 class='status'>Please do not close the browser tab, posts are started copying</h4>");
+      $('input[name="posts[]"]').each( function(index, value){
+        if($(value).prop("checked")){
+          $('.report .items-started').append('<li>' + $(value).val() + ' started to copying</li>');
+          $.post(ajax.ajaxurl, {
+            dataType: "json",
+            action: "copySinglePost",
+            formData: $(value).val(),
+            url: $("#url").val()
+          })
+          .then(function(response) {
+            $('.report .items-finished').append('<li>' + $(value).val() + ' ended copying as ' + response + ' </li>');
+          });
+        }
+      });
+    });
   })
 })(jQuery);
--- admin/gui/settings.php
+++ admin/gui/settings.php
@@ -1,7 +1,6 @@
 <?php 
 $url = isset($_SESSION['url'])? $_SESSION['url'] : '';
 $flag = false;
-$copyFlag = false;
 $list = [];
 if(isset($_POST['list'])){
   if( isset($_POST['url']) && ! empty($_POST['url']) ){
@@ -18,121 +17,117 @@ if(isset($_POST['list'])){
 
 $checkboxArray = [];
 $taxArray = [];
-  if( isset($_POST['submit']) && isset($_POST['posts'])){
+  // if( isset($_POST['submit']) && isset($_POST['posts'])){
 
-    $postIDArray = $_POST['posts'];
-    foreach($postIDArray as $postArrayKey => $postArrayValue){
-      $posts = json_decode(file_get_contents($url . "/wp-json/custom/v1/my-posts?key={$postArrayValue}" ));
-      if(! post_exists($posts[0]->post->post_title, '', '', $posts[0]->post_type)){
-        // Create the post first
-        $data = $posts[0];
-        $thisID = $data->post->ID;
-        foreach($data->post as $itemKey => $itemValue){
-          $postArray[$itemKey] = $itemValue; 
-        }
-        $previousID = $postArray['ID'];
-        $previousURL = $postArray['guid'];
+  //   $postIDArray = $_POST['posts'];
+  //   foreach($postIDArray as $postArrayKey => $postArrayValue){
+  //     $posts = json_decode(file_get_contents($url . "/wp-json/custom/v1/my-posts?key={$postArrayValue}" ));
+  //     if(! post_exists($posts[0]->post->post_title, '', '', $posts[0]->post_type)){
+  //       // Create the post first
+  //       $data = $posts[0];
+  //       $thisID = $data->post->ID;
+  //       foreach($data->post as $itemKey => $itemValue){
+  //         $postArray[$itemKey] = $itemValue; 
+  //       }
+  //       $previousID = $postArray['ID'];
+  //       $previousURL = $postArray['guid'];
 
-        unset($postArray['ID']); // Wordpress will assign a new Id
-        unset($postArray['guid']); // Wordpress will add new guid
+  //       unset($postArray['ID']); // Wordpress will assign a new Id
+  //       unset($postArray['guid']); // Wordpress will add new guid
 
-        /** upload respactive image inside post content */
-        $postContent = $postArray['post_content'];
-        preg_match_all('/<img[^>]+>/i',$postContent, $result); 
-        $img = array();
-        foreach( $result as $implementedResult){
-          foreach( $implementedResult as $img_tag){
-            preg_match_all('/(src=")([^"]*)"/i',$img_tag, $img[$img_tag]);
-          }
-        }
+  //       /** upload respactive image inside post content */
+  //       $postContent = $postArray['post_content'];
+  //       preg_match_all('/<img[^>]+>/i',$postContent, $result); 
+  //       $img = array();
+  //       foreach( $result as $implementedResult){
+  //         foreach( $implementedResult as $img_tag){
+  //           preg_match_all('/(src=")([^"]*)"/i',$img_tag, $img[$img_tag]);
+  //         }
+  //       }
         
-        $processedImg = array();
-        foreach($img as $imgKey => $imgTag){
-          $attachmentDetails = save_attachement( 
-            array( 'url' => preg_replace('/(-[0-9]*x[0-9]*)/m', '', end($imgTag)[0]), 'mainServerLocation' => $data->upload_dir ) 
-          );
-          if($attachmentDetails['id']){
-            $processedImg[end($imgTag)[0]] = $attachmentDetails['id'];
-          }else{
-            $processedImg[end($imgTag)[0]] = $attachmentDetails['url'];
-          }
-        }
-        foreach($processedImg as $mainLink => $actualLink){
-          $postContent = str_replace( $mainLink,$actualLink, $postContent);
-        }
-        $postArray['post_content'] = $postContent;
-        /** upload respactive image inside post content */
+  //       $processedImg = array();
+  //       foreach($img as $imgKey => $imgTag){
+  //         $attachmentDetails = save_attachement( 
+  //           array( 'url' => preg_replace('/(-[0-9]*x[0-9]*)/m', '', end($imgTag)[0]), 'mainServerLocation' => $data->upload_dir ) 
+  //         );
+  //         if($attachmentDetails['id']){
+  //           $processedImg[end($imgTag)[0]] = $attachmentDetails['id'];
+  //         }else{
+  //           $processedImg[end($imgTag)[0]] = $attachmentDetails['url'];
+  //         }
+  //       }
+  //       foreach($processedImg as $mainLink => $actualLink){
+  //         $postContent = str_replace( $mainLink,$actualLink, $postContent);
+  //       }
+  //       $postArray['post_content'] = $postContent;
+  //       /** upload respactive image inside post content */
         
-        $postArray['post_author'] = get_current_user_id(); // Current user will be the author of this post
-        $newID = wp_insert_post($postArray); // Inserting new post 
+  //       $postArray['post_author'] = get_current_user_id(); // Current user will be the author of this post
+  //       $newID = wp_insert_post($postArray); // Inserting new post 
 
-        $terms = $data->terms;
+  //       $terms = $data->terms;
         
-        foreach ($terms as $objectArrayKey => $objectArrayValue){
-          foreach($objectArrayValue as $key => $value){
-            if(term_exists( $value->slug, $value->taxonomy )){
-              $thisTerm = term_exists( $value->slug, $value->taxonomy );
-            }else{
-              $thisTerm = wp_insert_term($value->name, $value->taxonomy, array('description' => $value->description, 'parent' => $value->parent, 'slug' => $value->slug  ) );
-            }
-            wp_set_object_terms( $newID, (int)$thisTerm['term_id'], $value->taxonomy );
-          }
-        }
-
+  //       foreach ($terms as $objectArrayKey => $objectArrayValue){
+  //         foreach($objectArrayValue as $key => $value){
+  //           if(term_exists( $value->slug, $value->taxonomy )){
+  //             $thisTerm = term_exists( $value->slug, $value->taxonomy );
+  //           }else{
+  //             $thisTerm = wp_insert_term($value->name, $value->taxonomy, array('description' => $value->description, 'parent' => $value->parent, 'slug' => $value->slug  ) );
+  //           }
+  //           wp_set_object_terms( $newID, (int)$thisTerm['term_id'], $value->taxonomy );
+  //         }
+  //       }
         
-        // Now we have new post Id in $newID lets add all metas
-        add_post_meta( $newID, 'wp-copier', '1');
-        add_post_meta( $newID, 'previous-id', $previousID);
-        add_post_meta( $newID, 'previous-url', $previousURL);
-        add_post_meta( $newID, 'copy-from', $url);
+  //       // Now we have new post Id in $newID lets add all metas
+  //       add_post_meta( $newID, 'wp-copier', '1');
+  //       add_post_meta( $newID, 'previous-id', $previousID);
+  //       add_post_meta( $newID, 'previous-url', $previousURL);
+  //       add_post_meta( $newID, 'copy-from', $url);
 
-        $postMeta = $data->post_meta;
-        foreach($postMeta as $postMetaKey => $postMetaValue){
+  //       $postMeta = $data->post_meta;
+  //       foreach($postMeta as $postMetaKey => $postMetaValue){
 
-          // Lets replace the thumbnil or feature image first: 
-          if($postMetaKey === '_thumbnail_id'){ 
-            // get the thumbnil image src / url 
+  //         // Lets replace the thumbnil or feature image first: 
+  //         if($postMetaKey === '_thumbnail_id'){ 
+  //           // get the thumbnil image src / url 
             
-            $attachmentDetails = save_attachement( 
-              array( 'url' => $data->featured_img_src, 'id' => $postMetaValue[0], 'mainServerLocation' => $data->upload_dir ) 
-            );
-            add_post_meta($newID, $postMetaKey, $attachmentDetails['id']);
-          } 
+  //           $attachmentDetails = save_attachement( 
+  //             array( 'url' => $data->featured_img_src, 'id' => $postMetaValue[0], 'mainServerLocation' => $data->upload_dir ) 
+  //           );
+  //           add_post_meta($newID, $postMetaKey, $attachmentDetails['id']);
+  //         } 
 
-          // Now change the codestars meta boxs image links
-          else{
-            // By deafult all the meta are repreasnting by array
-            if(is_array($postMetaValue)){
+  //         // Now change the codestars meta boxs image links
+  //         else{
+  //           // By deafult all the meta are repreasnting by array
+  //           if(is_array($postMetaValue)){
               
-              foreach($postMetaValue as $innerArrayMetaValueKey => $innerArrayMetaValue ){
-                /** wordprress deafult metas will be theonly entry inside the array
-                  * but codestars meta could have multiple entry
-                  * in this loop all the meta will extract one by one inside the array, and will check if the value is serialized
-                  * If value is serialized then it is the value from codestrs framework
-                  * if we get any serialized value we will unseralize theme and loop each content for image or media url
-                  * if we find any we will replace the link and ID then serialize all data again and push to post meta
-                */
-                if(is_serialized( $innerArrayMetaValue )){
-                  $thisInnerArrayMetaValue = unserialize($innerArrayMetaValue);
-                  foreach($thisInnerArrayMetaValue as $metaValuename => $metaValueData){
-                    $thisInnerArrayMetaValue[$metaValuename] = processMetadatas($metaValueData, $data->upload_dir, $url);
-                    
-                  }
-                  add_post_meta($newID, $postMetaKey, $thisInnerArrayMetaValue);
-                }else{
-                  // If not serialized data
-                  add_post_meta($newID, $postMetaKey, $innerArrayMetaValue);
-                }
-              }
-            }
-          }
-        } 
-      }
-
-
-    }
-  }
-  if( ! $flag && ! $copyFlag){
+  //             foreach($postMetaValue as $innerArrayMetaValueKey => $innerArrayMetaValue ){
+  //               /** wordprress deafult metas will be theonly entry inside the array
+  //                 * but codestars meta could have multiple entry
+  //                 * in this loop all the meta will extract one by one inside the array, and will check if the value is serialized
+  //                 * If value is serialized then it is the value from codestrs framework
+  //                 * if we get any serialized value we will unseralize theme and loop each content for image or media url
+  //                 * if we find any we will replace the link and ID then serialize all data again and push to post meta
+  //               */
+  //               if(is_serialized( $innerArrayMetaValue )){
+  //                 $thisInnerArrayMetaValue = unserialize($innerArrayMetaValue);
+  //                 foreach($thisInnerArrayMetaValue as $metaValuename => $metaValueData){
+  //                   $thisInnerArrayMetaValue[$metaValuename] = processMetadatas($metaValueData, $data->upload_dir, $url);
+  //                 }
+  //                 add_post_meta($newID, $postMetaKey, $thisInnerArrayMetaValue);
+  //               }else{
+  //                 // If not serialized data
+  //                 add_post_meta($newID, $postMetaKey, $innerArrayMetaValue);
+  //               }
+  //             }
+  //           }
+  //         }
+  //       } 
+  //     }
+  //   }
+  // }
+  if( ! $flag ){
     unset($_SESSION['url']);
     unset($_SESSION['list']);
   }
@@ -198,6 +193,11 @@ $taxArray = [];
     <br> <br>
     <input type="submit" value="Start Copy" name="submit" style="position: fixed; bottom: 50px; right: 2%;">
   </form>
+
+  <div class="report">
+    <ul class="items-started"></ul>
+    <ul class="items-finished"></ul>
+  </div>
   <?php }  ?>
   </div>
 </div>
